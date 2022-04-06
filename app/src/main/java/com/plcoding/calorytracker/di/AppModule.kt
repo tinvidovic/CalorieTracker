d066b20879e1ca474970e71cc62e2f22f9d3aca7
tinvidovic <tin@vidovic.family>
2022-04-06 13:59:39 +0200

    Age Screen and ViewModel added

--- app/src/main/java/com/plcoding/calorytracker/di/AppModule.kt
+++ app/src/main/java/com/plcoding/calorytracker/di/AppModule.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.SharedPreferences
 import com.plcoding.core.data.preferences.DefaultPreferences
 import com.plcoding.core.domain.preferences.IPreferences
+import com.plcoding.core.domain.use_cases.FilterOutDigitsUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -30,5 +31,11 @@ class AppModule {
     fun providePreferences(sharedPreferences: SharedPreferences): IPreferences {
 
         return DefaultPreferences(sharedPreferences)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFilterOutDigitsUseCase(): FilterOutDigitsUseCase {
+        return FilterOutDigitsUseCase()
     }
 }
