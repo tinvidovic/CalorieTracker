31309a072be3c10b9e8b26de120f139b7cf08e97
tinvidovic <tin@vidovic.family>
2023-12-04 09:41:44 +0100

    Welcome screen UI

--- app/src/main/java/com/nticoding/calorytracker/MainActivity.kt
+++ app/src/main/java/com/nticoding/calorytracker/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.nticoding.calorytracker.ui.theme.CalorieTrackerTheme
+import com.nticoding.onboarding_presentation.welcome.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CalorieTrackerTheme {
-
+                WelcomeScreen()
             }
         }
     }
