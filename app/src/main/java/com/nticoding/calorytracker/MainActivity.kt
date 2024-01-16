7f1ed1b34f5f8ab49b31a8c7304b32a1344b8791
tinvidovic <tin@vidovic.family>
2024-01-16 10:15:11 +0100

    Add SearchTextField

--- app/src/main/java/com/nticoding/calorytracker/MainActivity.kt
+++ app/src/main/java/com/nticoding/calorytracker/MainActivity.kt
@@ -6,10 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.nticoding.calorytracker.ui.theme.CalorieTrackerTheme
 import com.nticoding.core.navigation.Route
 import com.nticoding.calorytracker.navigation.navigate
@@ -21,9 +24,11 @@ import com.nticoding.onboarding_presentation.goal.GoalScreen
 import com.nticoding.onboarding_presentation.height.HeightScreen
 import com.nticoding.onboarding_presentation.nutrient_goal.NutrientGoalScreen
 import com.nticoding.onboarding_presentation.weight.WeightScreen
+import com.nticoding.tracker_presentation.search.SearchScreen
 import com.nticoding.tracker_presentation.tracker_overview.TrackerOverviewScreen
 import dagger.hilt.android.AndroidEntryPoint
 
+@OptIn(ExperimentalComposeUiApi::class)
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -88,8 +93,36 @@ class MainActivity : ComponentActivity() {
                                 onNavigate = navController::navigate
                             )
                         }
-                        composable(Route.SEARCH) {
-
+                        composable(
+                            route = Route.SEARCH + "/{mealName}/{dayOfMonth}/{month}/{year}",
+                            arguments = listOf(
+                                navArgument("mealName") {
+                                    type = NavType.StringType
+                                },
+                                navArgument("dayOfMonth") {
+                                    type = NavType.IntType
+                                },
+                                navArgument("month") {
+                                    type = NavType.IntType
+                                },
+                                navArgument("year") {
+                                    type = NavType.IntType
+                                },
+                            )
+                        ) {
+                            val mealName = it.arguments?.getString("mealName")!!
+                            val dayOfMonth = it.arguments?.getInt("dayOfMonth")!!
+                            val month = it.arguments?.getInt("month")!!
+                            val year = it.arguments?.getInt("year")!!
+                            SearchScreen(
+                                scaffoldState = scaffoldState,
+                                mealName = mealName,
+                                dayOfMonth = dayOfMonth,
+                                month = month,
+                                year = year,
+                                onNavigateUp = {
+                                    navController.navigateUp()
+                                })
                         }
                     }
 
---
+++ tracker/tracker_presentation/src/main/java/com/nticoding/tracker_presentation/search/SearchScreen.kt
@@ -0,0 +1,76 @@
+package com.nticoding.tracker_presentation.search
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.nticoding.core.R as coreR
+import com.nticoding.core.util.UIEvent
+import com.nticoding.core_ui.localSpacing
+import com.nticoding.tracker_presentation.search.components.SearchTextField
+
+@ExperimentalComposeUiApi
+@Composable
+fun SearchScreen(
+    scaffoldState: ScaffoldState,
+    mealName: String,
+    dayOfMonth: Int,
+    month: Int,
+    year: Int,
+    onNavigateUp: () -> Unit,
+    viewModel: SearchViewModel = hiltViewModel()
+) {
+    val spacing = localSpacing.current
+    val state = viewModel.state
+    val context = LocalContext.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+    LaunchedEffect(key1 = keyboardController) {
+        viewModel.uiEvent.collect { event ->
+            when (event) {
+                is UIEvent.ShowSnackbar -> {
+                    scaffoldState.snackbarHostState.showSnackbar(
+                        message = event.message.asString(context)
+                    )
+                    keyboardController?.hide()
+                }
+                is UIEvent.NavigateUp -> onNavigateUp()
+                else -> Unit
+            }
+        }
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(spacing.spaceMedium)
+    ) {
+        Text(
+            text = stringResource(id = coreR.string.add_meal, mealName),
+            style = MaterialTheme.typography.h2
+        )
+        Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        SearchTextField(
+            text = state.query,
+            onValueChange = {
+                viewModel.onEvent(SearchEvent.OnQueryChange(it))
+            },
+            onSearch = {
+                viewModel.onEvent(SearchEvent.OnSearch)
+            },
+            onFocusChanged = {
+                viewModel.onEvent(SearchEvent.OnSearchFocusChange(it.isFocused))
+            }
+        )
+    }
+}
